dcd28970f95fce99c794e6fdc2f67baac333924c
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2025-06-07 21:41:00 +0200

    update / media queries

---
+++ assets/PageNotFound-285bfeea.js
@@ -0,0 +1,1 @@
+import{j as o,P as t}from"./index-216caa39.js";function e(){return o.jsxs("div",{children:[o.jsx(t,{}),"PageNotFound"]})}export{e as default};
---
+++ assets/Product.module-27790b01.js
@@ -0,0 +1,1 @@
+const o="_product_o1woa_1",t={product:o};export{t as s};
