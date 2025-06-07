c0559bfff000d435ebfb8dc5e68d6ec4a9190e53
ViniciusJMoreira <101931545+ViniciusJMoreira@users.noreply.github.com>
2025-06-07 22:05:07 +0200

    update

---
+++ assets/PageNotFound-7ce9395a.js
@@ -0,0 +1,1 @@
+import{j as o,P as t}from"./index-a115b5c1.js";function e(){return o.jsxs("div",{children:[o.jsx(t,{}),"PageNotFound"]})}export{e as default};
---
+++ assets/Product.module-92797e3c.js
@@ -0,0 +1,1 @@
+const c="_product_c2k2z_1",t="_img_c2k2z_29",o={product:c,img:t};export{o as s};
